51597c446ebbd78b42f2524c911a0b529d171d6f
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-12-31 21:08:50 +0000

    Final commit before new year

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,39 +3,43 @@
 import { ClerkLoaded, SignInButton, UserButton, useUser } from "@clerk/nextjs"
 import Link from "next/link";
 import Form from "next/form";
-import { PackageIcon, TrolleyIcon } from "@sanity/icons";
+import { PackageIcon, SearchIcon, TrolleyIcon } from "@sanity/icons";
 import { SignedIn } from "@clerk/clerk-react";
 
 
 export default function Header() {
     const {user} = useUser();
-    const createClerkPasskey = async() => {
- try {
-  const response = await user?.createPasskey()
-  console.log(response);
+//     const createClerkPasskey = async() => {
+//  try {
+//   const response = await user?.createPasskey()
+//   console.log(response);
   
- } catch (err) {
-  console.error("Error:", JSON.stringify(err, null, 2))
- }
-    }
+//  } catch (err) {
+//   console.error("Error:", JSON.stringify(err, null, 2))
+//  }
+//     }
 
   return (
-    <header className="flex flex-wrap justify-between items-center py-4 px-6">
+    <header className="flex flex-wrap bg-blue-950 fixed w-full justify-between items-center py-4 px-6">
     {/* Top row of the Header */}
     <div className="flex w-full flex-wrap justify-between items-center">
-        <Link href="/" className="text-2xl font-bold text-blue-700 cursor-pointer mx-auto sm:mx-0">
+        <Link href="/" className="text-2xl font-bold text-gray-300 cursor-pointer mx-auto sm:mx-0">
             NovaMart
         </Link>
         <Form
             action="/search" 
             className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
+            
         >
+          
             <input
                 type="text"
                 name="query"
                 placeholder="Search for products"
-                className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl"
+                className="bg-gray-300 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-30 border w-full max-w-4xl"
+                
             />
+            
         </Form>
 
  {/* Left side of the header */}
@@ -43,7 +47,7 @@ export default function Header() {
         <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
         <Link
             href="/cart"
-            className="flex relative flex-1 justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="flex relative flex-1 justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-white hover:bg-blue-950 hover:text-white hover:border text-blue-950 font-bold py-2 px-4 rounded"
         >
             <TrolleyIcon className="w-6 h-6"/>
             {/* Span items count when the global state is implemented */}
@@ -54,7 +58,7 @@ export default function Header() {
         <ClerkLoaded>
           <SignedIn>
             <Link href="/order" 
-            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 font-bold py-2 px-4 rounded  bg-white hover:bg-blue-950 hover:text-white hover:border text-blue-950"
             >
               <PackageIcon className="w-6 h-6"/>
               <span>Orders</span>
@@ -65,21 +69,22 @@ export default function Header() {
             <div className="flex gap-4 items-center justify-center">
               <UserButton />
               <div className="hidden sm:block text-sx">
-                <p className="text-gray-400">Welcome Back!</p>
-                <p className="font-bold">{user.fullName}</p>
+                <p className="text-gray-300">Welcome Back!</p>
+                <p className="font-bold text-gray-200 ">{user.fullName}</p>
               </div>
             </div>
            ): (
             <SignInButton mode="modal"/>
            )}
-          {user?.passkeys.length === 0 && (
+           {/* User Pass Key */}
+          {/* {user?.passkeys.length === 0 && (
             <button
             onClick={createClerkPasskey}
               className="bg-white hover:bg-blue-700 text-blue-500 hover:text-white animate-pulse font-bold py-2 px-2 border-blue-300 border"
             >
              Create passkey 
             </button>
-          )}
+          )} */}
 
         </ClerkLoaded>
         </div>
