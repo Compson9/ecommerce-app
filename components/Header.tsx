abb75770facdd204188082f88efc68ebbc07fd0f
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-01-01 04:11:20 +0000

    We will continue tomorrow

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,16 +28,11 @@ export default function Header() {
         </Link>
         
 <Form action="/search" className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0">   
-    <label  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div className="relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-        </div>
-        <input type="text" name="query" className="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-blue-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..."  />
-        <button type="submit" className="text-black absolute end-2.5 bottom-2.5 bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 p-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
+<div className="bg-white w-full flex px-1 py-1 rounded-full border border-blue-500 overflow-hidden ">
+        <input type='text' name="query" placeholder='Search for products...' className="w-full p2 outline-none bg-white pl-4 text-sm" />
+        <button type='button'
+          className="bg-blue-950 hover:bg-blue-900 transition-all text-white text-sm rounded-full px-5 py-2.5">Search</button>
+      </div>
 </Form>
 
         {/* <Form
