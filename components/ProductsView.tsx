8585459ac50f615de9691e90884a1c8c974b92a3
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-01-08 23:14:32 +0000

    The commits made

--- components/ProductsView.tsx
+++ components/ProductsView.tsx
@@ -6,6 +6,9 @@ interface ProductViewProps {
     categories: Category[],
 }
 
+
+
+
 export default function ProductView({ products,  }: ProductViewProps) {
     return (
         <div className="flex flex-col">
@@ -13,12 +16,10 @@ export default function ProductView({ products,  }: ProductViewProps) {
             <div className="w-full sm:w-[200px]">
                 {/* <CategorySelectorComponent categories={categories}/> */}
             </div>
-
             {/* Products Fetch */}
             <div className="">
                 <div>
                     <ProductGrid products={products} />
-
                     {/* <hr className="w-1/2 sm:w-3/4" /> */}
                 </div>
             </div>
