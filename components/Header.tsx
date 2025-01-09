df16074e205cc0c5730e52e10a9de8038d460bfe
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-01-09 15:17:03 +0000

    Final Commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
 //     }
 
   return (
-    <header className="flex z-30 flex-wrap bg-blue-950 fixed w-full justify-between items-center py-4 px-6">
+    <header className="flex z-30 flex-wrap bg-gray-900 fixed w-full justify-between items-center py-4 px-6">
     {/* Top row of the Header */}
     <div className="flex w-full flex-wrap justify-between items-center">
         <Link href="/" className="text-2xl font-bold text-gray-300 cursor-pointer mx-auto sm:mx-0">
@@ -31,7 +31,7 @@ export default function Header() {
 <div className="bg-white w-full flex px-1 py-1 rounded-full border border-blue-500 overflow-hidden ">
         <input type='text' name="query" placeholder='Search for products...' className="w-full p2 outline-none bg-white pl-4 text-sm" />
         <button type='button'
-          className="bg-blue-950 hover:bg-blue-900 transition-all text-white text-sm rounded-full px-5 py-2.5">Search</button>
+          className="bg-gray-900 hover:bg-gray-800 transition-all text-white text-sm rounded-full px-5 py-2.5">Search</button>
       </div>
 </Form>
 
@@ -54,7 +54,7 @@ export default function Header() {
         <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
         <Link
             href="/cart"
-            className="flex relative flex-1 justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-white hover:bg-blue-950 hover:text-white hover:border text-blue-950 font-bold py-2 px-4 rounded"
+            className="flex relative flex-1 justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-white hover:bg-gray-800 hover:text-white hover:border text-blue-950 font-bold py-2 px-4 rounded"
         >
             <TrolleyIcon className="w-6 h-6"/>
             {/* Span items count when the global state is implemented */}
@@ -65,7 +65,7 @@ export default function Header() {
         <ClerkLoaded>
           <SignedIn>
             <Link href="/order" 
-            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 font-bold py-2 px-4 rounded  bg-white hover:bg-blue-950 hover:text-white hover:border text-blue-950"
+            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 font-bold py-2 px-4 rounded  bg-white hover:bg-gray-800 hover:text-white hover:border text-bl"
             >
               <PackageIcon className="w-6 h-6"/>
               <span>Orders</span>
