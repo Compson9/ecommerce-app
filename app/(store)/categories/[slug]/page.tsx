27ce7884a6fe15cedc2ddd02ef2163bba7449ca7
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-01-12 22:02:57 +0000

    Made a loading component fo rthe search page

--- app/(store)/categories/[slug]/page.tsx
+++ app/(store)/categories/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import ProductView from "@/components/ProductsView";
 import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import { getProductByCategory } from "@/sanity/lib/products/getProductByCategory";
 
@@ -5,8 +6,8 @@ export default async function CategoryPage(
     {params}: {params: Promise<{slug:string}>}) 
 {
     const {slug} = await params;
-    const category = getProductByCategory(slug);
-    const categories = await getAllCategories()
+    const products = getProductByCategory(slug);
+    const categories = await getAllCategories();
 
 
 
@@ -15,7 +16,14 @@ export default async function CategoryPage(
   return (
     <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-20">
         <div className="text-3xl font-bold mb-6 text-center">
-
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        {slug
+        .split('')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1) )
+        }
+        Collection
+      </h1>
+      <ProductView products={products} categories={categories} />
         </div>
 
     </div>
