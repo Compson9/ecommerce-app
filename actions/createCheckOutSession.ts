c1ee9e1ae14f862ed9b6e815357d164e9d963cb4
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-01-17 22:50:36 +0000

    Working on the stripe payment session

--- actions/createCheckOutSession.ts
+++ actions/createCheckOutSession.ts
@@ -1,5 +1,6 @@
 "use server "
 
+import stripe from "@/lib/stripe";
 import { BasketItem } from "@/store/store";
 
 export type Metadata = {
@@ -25,6 +26,38 @@ export async function createCheckOutSession(
         if(itemWithoutPrice.length > 0) {
             throw new Error("Cannot checkout with an item without price")
         }
+
+        // check for existing customer email 
+        const customers = await stripe.customers.list({
+            email: metadata.customerEmail,
+            limit: 1
+        });
+
+        let customerId: string | undefined;
+        if(customers.data.length > 0){
+            customerId = customers.data[0].id;
+        }
+
+        const session = await stripe.checkout.sessions.create({
+            customer: customerId,
+            customer_creation: customerId ? undefined : "always",
+            customer_email: !customerId ? metadata.customerEmail : undefined,
+            metadata,
+            mode: "payment",
+            allow_promotion_codes: true,
+            success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}&orderName=${metadata.orderNumber}`,
+            cancel_url: `${`https://${process.env.VERCEL_URE}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+            line_items: items.map((item)=> (
+                {
+                    quantity: item.quantity,
+                    price_data: {
+                        currency: "cedis",
+                        unit_amount: Math.round(item.product.price! * 100),
+                        
+                    }
+                }
+            ))
+        })
         
     } catch (error) {
         console.log("Error creating checkout session", error);
---
+++ lib/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from "stripe"
+// check if stripe is available
+if(!process.env.STRIPE_SECRET_KEY){
+    throw new Error("Missing STRIPE_SECRET_KEY environment variable")
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2024-12-18.acacia" 
+
+})
+
+export default stripe
