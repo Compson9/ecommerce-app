ebf877389043a7f8b040dd66c8073074b2fea142
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-01-19 00:59:55 +0000

    second commit

--- app/(store)/webhook/route.ts
+++ app/(store)/webhook/route.ts
@@ -1,6 +1,10 @@
+import { Metadata } from "@/actions/createCheckOutSession";
 import { headers } from "next/headers"
 import {NextRequest, NextResponse} from "next/server"
 import Stripe from "stripe";
+import stripe from "@/lib/stripe"
+
+
 
 export async function POST(req: NextRequest){
     const body = await req.text();
@@ -28,8 +32,49 @@ export async function POST(req: NextRequest){
 
     if(event.type === "checkout.session.completed"){
         const session = event.data.object as Stripe.Checkout.Session;
+
+        try {
+            const  order = await createOrderInSanity(session);
+            console.log("Order created in Sanity", order);
+        } catch (error) {
+            console.log("Error creating order in Sanity", error);
+            return NextResponse.json({error: `Error creating order: ${error}`},
+                {status: 500});
+    
+        }
     }
 
-    
+    return NextResponse.json({received: true});
+
+}
+
+async function createOrderInSanity(session: Stripe.Checkout.Session){
+
+    const {
+        id,
+        amount_total,
+        currency,
+        metadata,
+        payment_intent,
+        customer,
+        total_details
+    } =session;
+
+    // Create an order in your own database or Sanity CMS here
+    const {orderNumber, customerName, customerEmail, clerkUserId} = metadata as Metadata;
+
+    const lineItemsWithProduct = await stripe.checkout.sessions.listLineItems(
+        id, 
+        {
+            expand: ["order.price.product"],
+        }
+     );
+     const sanityProducts = lineItemsWithProduct.data.map((item)=> ({
+        _key: crypto.randomUUID(),
+        product: {
+            _type: "reference",
+            _ref: (item.price?.product as Stripe.Product)?.metadata?.id
+        }
+     }));
 
 }
