b601c1cb3cb243bec197a396993bf9ee3e3357e6
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-01-25 09:36:41 +0000

    Order page still not working

--- app/(store)/orders/page.tsx
+++ app/(store)/orders/page.tsx
@@ -13,7 +13,7 @@ export default async function ordersPage(){
         return redirect("/")
     }
 
-    const orders = await getMyOrders(userId);
+    const order = await getMyOrders(userId);
 
 
     
@@ -24,14 +24,43 @@ export default async function ordersPage(){
                 <h1 className="text-2xl font-bold text-gray-900 tracking-tight mb-8">
                     My Orders
                 </h1>
-                {orders.length === 0 ? (
+                {order.length === 0 ? (
                     <div className="text-center text-gray-600">
                         <p>You have not placed any orders yet.</p>
                     </div>
 
                 ): (
-                    <div className="space-y-4">
-                        <h1>Order</h1>
+                    <div className="space-y-4 sm:space-y-8">
+                        {order.map((order)=> (
+                            <div
+                            key={order.orderNumber}
+                            className="bg-white border border-gray-200 rounded-lg shadow-sm overflow-hidden"
+                            >
+                            <div className="p-4 sm:p-6 border-gray-200">
+                                <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between mb-4">
+                                    <div>
+                                        <p className="text-sm text-gray-600 mb-1 font-bold">
+                                            order Number
+                                        </p>
+                                        <p className="font-mono text-sm text-green-600 break-all">
+                                            {order.orderNumber}
+                                        </p>
+                                    </div>
+                                    <div className="sm:text-right">
+                                        <p className="text-sm-gray-600 mb-1">Order Date</p>
+                                        <p className="font-medium">
+                                            {order.orderDate ? new Date(order.orderDate).toLocaleDateString() : "N/A"}
+
+                                        </p>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                            </div>
+
+                        ))}
+                        
                     </div>
                 )}
             </div> 
